d6c457479dd12d408e43aee0005af522025385ff
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-02-26 15:46:08 +0530

    Added requireAuth middleware for route-auth

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,13 +1,17 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 
 import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentUser', currentUser, (req, res) => {
-    res.send({ currentUser: req.currentUser || null });
-});
+router.get('/api/users/currentUser',
+    currentUser,
+    requireAuth,
+    (req, res) => {
+        res.send({ currentUser: req.currentUser || null });
+    }
+);
 
 export { router as currentUserRouter };
 
