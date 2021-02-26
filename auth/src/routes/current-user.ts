482260ef8d54170cbd585d54d56c2cc23c5847a6
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-02-26 15:22:26 +0530

    implement /api/users/currentUser

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentUser', (req, res) => {
-    res.send('Hi there!');
+    if (!req.session?.jwt) {
+        return res.send({ currentUser: null });
+    }
+
+    try {
+        const payload = jwt.verify(
+            req.session.jwt,
+            
+            // used ! to tell ts not to worry whether this is defined or not
+            process.env.JWT_KEY! 
+        );
+        res.send({ currentUser: payload });
+    } catch (err) {
+        res.send({ currentUser: null});
+    } 
 });
 
 export { router as currentUserRouter };
