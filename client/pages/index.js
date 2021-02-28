ff4bab190f134b93bb84f42cabfd6e699dc28060
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-02-28 16:57:29 +0530

    Added a resuable api client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
     console.log(currentUser);
@@ -7,22 +7,11 @@ const LandingPage = ({ currentUser }) => {
 };
 
 // This will be executed during the server side rendering process
-LandingPage.getInitialProps = async () => {
-    if (typeof window === 'undefined') {
-        // we are on the server
-        const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-            {
-                headers: {
-                    Host: 'ticketing.dev'
-                }
-            }
-        );
-        return data;
-    } else {
-        // we are on the browser
-        const { data } = await axios.get('/api/users/currentuser');
-        return data;
-    }
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser');
+
+    return data;
 };
 
 export default LandingPage;
