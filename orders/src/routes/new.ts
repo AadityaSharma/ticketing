f32a1f222339a5fa1d315afa3cf7b3b8c73aea29
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-03-06 20:07:13 +0530

    order expiration time

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -7,6 +7,8 @@ import { Order } from '../models/order';
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60; // 15 MINUTES
+
 router.post('/api/orders',
     requireAuth,
     [
@@ -33,15 +35,21 @@ router.post('/api/orders',
         }
 
         // Calculate an expiration date for this order
-
+        const expiration = new Date();
+        expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
         // Build the order and save it to the database
+        const order = Order.build({
+            userId: req.currentUser!.id,
+            status: OrderStatus.Created,
+            expiresAt: expiration,
+            ticket
+        });
+
+        //Todo: Publish an event saying that an order was created
 
 
-        //Publish an event saying that an order was created
-
-
-        res.send({});
+        res.status(201).send(order);
     }
 );
 
