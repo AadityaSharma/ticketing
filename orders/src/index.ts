a4704ba5abefe98d0b092fbd01390cce2cfb39c8
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-03-07 11:51:43 +0530

    listeners for ticket creation and updation events

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 import { natsWrapper } from './nats-wrapper';
-
 import { app } from './app';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
@@ -38,6 +39,9 @@ const start = async () => {
         // Watching for terminate signals
         // stan.close() would close down our client
         process.on('SIGTERM', () => natsWrapper.client.close());
+
+        new TicketCreatedListener(natsWrapper.client).listen();
+        new TicketUpdatedListener(natsWrapper.client).listen();
 
         await mongoose.connect(process.env.MONGO_URI, { 
             useNewUrlParser: true,
