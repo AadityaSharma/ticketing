cacd0c5036545224c533e3ca3681165ccf9defeb
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-03-09 16:41:52 +0530

    Included update-if-current plugin to Order model

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -1,4 +1,5 @@
 import { OrderStatus } from '@aadi-tickets/common';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import mongoose from 'mongoose';
 
 interface OrderAttrs {
@@ -41,6 +42,9 @@ const orderSchema = new mongoose.Schema({
         }
     }
 });
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
     return new Order({
